443f66556732fab966779e2af481ecca84548f31
Leonard Thomas <leo.thomas@me.com>
2019-04-17 22:50:35 +0200

    improved width for smaller percentages

--- Sources/FlameGraph/ImageRenderer.swift
+++ Sources/FlameGraph/ImageRenderer.swift
@@ -7,12 +7,12 @@ class ImageRenderer {
         return NSImage(size: NSSize(width: 1000, height: maxDepth * cellHeight), flipped: false) { (rect) -> Bool in
             let context = NSGraphicsContext.current!.cgContext
 
-            renderLayer(nodes: graph.subNodes, context: context, totalWidth: rect.width, y: 0, x: 0, maxPercentage: graph.symbol.percentage, height: cellHeight)
+            renderLayer(nodes: graph.subNodes, context: context, totalWidth: rect.width, parentWidth: rect.width, y: 0, x: 0, maxPercentage: graph.symbol.percentage, height: cellHeight)
             return true
         }
     }
 
-    private class func renderLayer(nodes: [CallGraphNode], context: CGContext, totalWidth: CGFloat, y: CGFloat, x: CGFloat, maxPercentage: Float, height: CGFloat) {
+    private class func renderLayer(nodes: [CallGraphNode], context: CGContext, totalWidth: CGFloat, parentWidth: CGFloat, y: CGFloat, x: CGFloat, maxPercentage: Float, height: CGFloat) {
         let colors: [NSColor] = [
             #colorLiteral(red: 0.1921568662, green: 0.007843137719, blue: 0.09019608051, alpha: 1),
             #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1),
@@ -28,7 +28,11 @@ class ImageRenderer {
         let ySpacing: CGFloat = 1
         var currentX: CGFloat = x
         for node in nodes {
-            let currentWidth = max(totalWidth * CGFloat(node.symbol.percentage / maxPercentage), 2)
+            var currentWidth = totalWidth * CGFloat(node.symbol.percentage / maxPercentage)
+            if currentWidth < 1 {
+                currentWidth = min(parentWidth / CGFloat(node.parentNode?.subNodes.count ?? 1), 1)
+            }
+
             let rect = CGRect(x: currentX, y: y, width: currentWidth, height: height)
             colors.randomElement()?.setFill()
             context.fill(rect)
